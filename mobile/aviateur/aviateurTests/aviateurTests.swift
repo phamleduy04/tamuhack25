23fee54207561be388f2cf83cfc1fa8064f87bee
Ryan Lahlou <lryanlle@gmail.com>
2025-01-25 14:47:19 -0600

    feat: mobile scaffolding

---
+++ mobile/aviateur/aviateurTests/aviateurTests.swift
@@ -0,0 +1,17 @@
+//
+//  aviateurTests.swift
+//  aviateurTests
+//
+//  Created by Ryan Lahlou on 1/25/25.
+//
+
+import Testing
+@testable import aviateur
+
+struct aviateurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
